d31d34c4e7d6217679a3692eaab39c2de2a67651
Juani <nacho1294.ji@gmail.com>
2023-07-04 18:22:13 -0300

    la primera subida de mi portafolio

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import NavLinks from "./components/NavLinks";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
+
+export default function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <NavLinks />
+      <SocialLinks />
+    </div>
+  );
+}
