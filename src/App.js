dd6b9616e6665479d5fce2f31920bb6f58c52d5f
Juan Ignacio Alvarez <nacho1294.ji@gmail.com>
2024-02-06 17:09:22 -0300

    Modificacion para despliege y ha funcionado bien

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AcercaDe from "./components/AcercaDe";
 import Contacto from "./components/Contacto";
 import Experience from "./components/Experience";
@@ -9,9 +9,32 @@ import SocialLinks from "./components/SocialLinks";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import Footer from "./components/Footer";
 import Inicio from "./components/Inicio";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    // Configurar Firebase
+    const firebaseConfig = {
+      apiKey: "AIzaSyB1XwviAGnVHx8rpjfCMK9s_3rWa1F9S1M",
+      authDomain: "my-portafolio-5b6df.firebaseapp.com",
+      projectId: "my-portafolio-5b6df",
+      storageBucket: "my-portafolio-5b6df.appspot.com",
+      messagingSenderId: "627951744726",
+      appId: "1:627951744726:web:57ba7fb9014839d783bb77",
+      measurementId: "G-MCXR9KCDM1"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+    // Limpieza al desmontar el componente
+    return () => {
+      // Código de limpieza si es necesario
+    };
+  }, []); // El array vacío asegura que este efecto se ejecute solo una vez al montar el componente
 
   function toggleDarkMode() {
     setDarkMode(prevDarkMode => !prevDarkMode);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// Crear el "root" para renderizar la aplicación React en el DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Renderizar la aplicación React
 root.render(
   <React.StrictMode>
     <App />
